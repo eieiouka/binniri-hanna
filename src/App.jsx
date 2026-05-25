5192c4c16accda0dbd0d104681a4dd04002c6f43
eieiouka <eieiyukkuri@gmail.com>
2026-05-26 01:02:38 +0900

    j

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import PuzzleBoard from "./components/PuzzleBoard/PuzzleBoard";
 function App() {
   return (
     <div className="app">
+      <div className="game-title">
+        <span className="title-green">
+          遠野ハンナ
+        </span>
+
+        <span className="title-rest">
+          を砂時計から救出しよう！
+        </span>
+      </div>
+
       <main className="app-main">
         <PuzzleBoard />
       </main>
